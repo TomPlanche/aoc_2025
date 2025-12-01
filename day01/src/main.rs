1c9125c0adb35ff97fc789535f230993a40e785e
Tom Planche <tomplanche@proton.me>
2025-12-01 14:42:55 +0100

    [2] (feat on main) D1-P1

---
+++ day01/src/main.rs
@@ -0,0 +1,54 @@
+use utils::{run_solution, Solution};
+
+struct Day01;
+
+impl Solution for Day01 {
+    type Input = Vec<(char, i32)>;
+    type Output = i32;
+
+    fn parse_input(&self, input: &str) -> Self::Input {
+        input
+            .lines()
+            .filter_map(|line| {
+                let line = line.trim();
+                if line.is_empty() {
+                    return None;
+                }
+                let direction = line.chars().next()?;
+                let distance = line[1..].parse().ok()?;
+                Some((direction, distance))
+            })
+            .collect()
+    }
+
+    fn part1(&self, data: &Self::Input) -> Self::Output {
+        let mut position = 50;
+        let mut count = 0;
+
+        for &(direction, distance) in data {
+            match direction {
+                'L' => {
+                    position = (position - distance).rem_euclid(100);
+                }
+                'R' => {
+                    position = (position + distance) % 100;
+                }
+                _ => {}
+            }
+
+            if position == 0 {
+                count += 1;
+            }
+        }
+
+        count
+    }
+
+    fn part2(&self, _data: &Self::Input) -> Self::Output {
+        0
+    }
+}
+
+fn main() {
+    run_solution!(Day01);
+}
