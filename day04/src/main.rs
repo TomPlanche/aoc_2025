e600181fca631f731f6e72872e04695c5f02c6a5
Tom Planche <tomplanche@proton.me>
2025-12-04 09:39:42 +0100

    [15] (chore on main) D4 init

---
+++ day04/src/main.rs
@@ -0,0 +1,58 @@
+use utils::{run_solution, Solution};
+
+struct Day04;
+
+impl Solution for Day04 {
+    type Input = Vec<i32>;
+    type Output = i32;
+
+    fn parse_input(&self, input: &str) -> Self::Input {
+        input
+            .lines()
+            .filter_map(|line| line.parse().ok())
+            .collect()
+    }
+
+    fn part1(&self, _data: &Self::Input) -> Self::Output {
+        // TODO: Implement part 1
+        0
+    }
+
+    fn part2(&self, _data: &Self::Input) -> Self::Output {
+        // TODO: Implement part 2
+        0
+    }
+}
+
+fn main() {
+    run_solution!(Day04);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input = "";
+
+        let day = Day04;
+        let parsed_input = day.parse_input(input);
+
+        let part1 = day.part1(&parsed_input);
+
+        assert_eq!(part1, 0);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = "";
+
+        let day = Day04;
+        let parsed_input = day.parse_input(input);
+
+        let part2 = day.part2(&parsed_input);
+
+        assert_eq!(part2, 0);
+    }
+}
